d5c4ef8adc01e33dee53680de84d80b69d3f1313
wanggang <wg421338916@163.com>
2019-04-11 17:06:05 +0800

    some msg

---
+++ threaddemo/src/main/java/CountDownLatchDemo.java
@@ -0,0 +1,25 @@
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+public class CountDownLatchDemo {
+    public static void main(String[] args) throws InterruptedException {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(2);
+        IntStream.range(0, 2).forEach(i -> {
+            new Thread(() -> {
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("ok" + Thread.currentThread().getName());
+            }).start();
+        });
+
+        TimeUnit.SECONDS.sleep(2);
+    }
+}
